3d9858e10c24147c983361a4912d32769cda8b4b
Matthew <siunami.matt@gmail.com>
2018-05-17 13:08:14 -0400

    clean up attest action

--- src/user/ui/attestbutton/AttestButtonActions.js
+++ src/user/ui/attestbutton/AttestButtonActions.js
@@ -1,18 +1,9 @@
 import { browserHistory } from 'react-router'
 import { uport } from './../../../util/connectors.js'
 
-// export const USER_LOGGED_OUT = 'USER_LOGGED_OUT'
-// function userLoggedOut(user) {
-//   return {
-//     type: USER_LOGGED_OUT,
-//     payload: user
-//   }
-// }
-
 export function attestUser() {
   return function(dispatch) {
     uport.requestCredentials().then((credentials) => {
-      console.log(credentials);
       // FUTURE TODO: Check credentials with event database before offering attestation.
 
       // Attest specific credentials
@@ -25,8 +16,6 @@ export function attestUser() {
         exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
       })
     })
-    // // Logout user.
-    // dispatch(userLoggedOut())
 
     // Redirect home.
     return browserHistory.push('/')
